ae53a1b27ee594a811716e7fa86646d6f858d22e
Tsyriulnikov <max_djvu@mail.ru>
2022-02-20 15:54:49 +0300

    10 day

---
+++ jsads08.js
@@ -0,0 +1,15 @@
+/*var arr = [6, 89, 3, 45];
+var maximus = Math.max.apply(null, arr);
+maximus would have a value of 89.
+
+We had to use Math.max.apply(null, arr) because Math.max(arr) returns NaN. Math.max() expects comma-separated arguments, but not an array. The spread operator makes this syntax much better to read and maintain.
+
+const arr = [6, 89, 3, 45];
+const maximus = Math.max(...arr);
+*/
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [...arr1];  // Change this line
+
+console.log(arr2);
